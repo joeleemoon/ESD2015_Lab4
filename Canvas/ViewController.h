e110100db0322dc05271260aa1568508d401ea01
Lee Joe <joelee@Lees-Mac.local>
2015-04-02 20:53:14 +0800

    Initial Commit

---
+++ Canvas/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Canvas
+//
+//  Created by Lee Joe on 4/2/15.
+//  Copyright (c) 2015 Lee Joe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Canvas/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Canvas
+//
+//  Created by Lee Joe on 4/2/15.
+//  Copyright (c) 2015 Lee Joe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
